d2e306ba937e15b52aab1cab35b21a86ed6f44ba
Tri-Hartono <trihartono293@gmail.com>
2023-08-05 02:37:48 +0700

    commit

--- app/header/index.js
+++ app/header/index.js
@@ -1,21 +1,27 @@
 'use client';
 import Image from 'next/image';
-import React from 'react';
 import { HandIcon } from '../../assets/images';
 import { HiOutlineChevronDown, HiOutlineArrowCircleDown } from 'react-icons/hi';
 import ButtonProps from '../../components/button';
 import { motion } from 'framer-motion';
 import Box from '../../components/Svg/box';
 import { Link } from 'react-scroll';
+import Circle from '../../components/Svg/circle';
+
 export default function Header() {
   return (
     <motion.div animate={{ x: 0 }} initial={{ x: '-100%' }} transition={{ duration: 1 }} id="/" className="py-10 md:py-20 flex flex-col  justify-center relative ">
       <div className="md:w-[75%] space-y-4 relative z-20 ">
         <div className="flex items-center gap-2">
           <h3>Hi,</h3>
-          <div className="h-5 w-5">
+          <motion.div
+            animate={{ rotate: [0, 30, 0, -30, 0] }} // Rotasi gambar tangan secara berulang
+            transition={{ duration: 2, repeat: Infinity }}
+            className="h-5 w-5"
+          >
             <Image src={HandIcon} width="" height="" alt="icon hand" />
-          </div>
+          </motion.div>
+
           <h3>My name is</h3>
         </div>
         <div className="text-5xl font-bold">Tri Hartono</div>
@@ -25,11 +31,14 @@ export default function Header() {
           I am a frontend developer who has responsive web design skills and beautiful UI, I like to learn new things according to my field. For me the job is a responsibility that is carried out carefully and to the maximum extent
           possible, both personal and team work
         </p>
-        <div className="py-4 w-fit ">
+        <div className="py-4 w-fit relative ">
           <Link to="contact" spy={true} activeClass="active" smooth={true} offset={-100} className="cursor-pointer">
             <ButtonProps name="Hire Me" icon={<HiOutlineChevronDown />}></ButtonProps>
           </Link>
         </div>
+        {/* <div className="absolute -left-32 -bottom-24 md:-left-52 md:-bottom-20 -z-12">
+          <Circle />
+        </div> */}
       </div>
       <div className="absolute z-0 -top-1/4 opacity-10 h-4/5 -right-1/4 w-3/4 md:h-full ">
         <div className="w-1/2 -left-1/4 rounded-3xl z-10 -rotate-12 h-full bg-lightColors dark:bg-darkColors absolute"></div>
